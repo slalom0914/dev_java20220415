fcb74f28f9692346b459a0500cbbd58d947114fa
slalom <slalom0914@gmail.com>
2022-04-19 20:32:24 +0900

    static 예제추가2

--- dev_java/src/com/basic/StaticTest_1.java
+++ dev_java/src/com/basic/StaticTest_1.java
@@ -10,6 +10,8 @@ public class StaticTest_1 {
 	// 메인메소드
 	public static void main(String[] args) {
 		StaticTest_1 st1 = new StaticTest_1();
+	// 내 안에 있는 메소드라 하더라도 메소드 선언시 static이 붙어 있지 않으면
+	// 인스턴스화를 해야 호출할 수 있다	
 		st1.methodA();
 	}
 
