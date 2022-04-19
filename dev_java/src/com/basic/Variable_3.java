9a2011fdb982cd86592099c97cef140fcb0fcf0f
slalom <slalom0914@gmail.com>
2022-04-19 21:24:57 +0900

    전역변수와 지역변수 활용1

---
+++ dev_java/src/com/basic/Variable_3.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class Variable_3 {
+	int age = 20;
+	void methodA(int age) {
+		age = 30;
+		System.out.println("methodA : "+age);
+	}
+	public static void main(String[] args) {
+		Variable_3 v1 = new Variable_3();
+		System.out.println("before : "+v1.age);
+		v1.methodA(50);
+		System.out.println("after : "+v1.age);		
+	}
+}
