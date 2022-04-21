e10c571426aab7813d0df7ebcfd4894a0d36f182
slalom <slalom0914@gmail.com>
2022-04-21 15:15:41 +0900

    2022년 4월21일 수업소스 반영

--- dev_java/src/com/day4/Variable_1.java
+++ dev_java/src/com/day4/Variable_1.java
@@ -2,13 +2,15 @@ package com.day4;
 
 public class Variable_1 {
 	// 전역변수 age선언 및 초기화(초기값은 20임)
-	int age = 20;
+	int age = 20;// 전역변수는 인스턴스화가 실행되고 나면 그 다음에 heap메모리 안에 클래스 안에 초기화가 된다
 	// 사용자 정의 메소드 선언 - 파라미터는 없어도 합법, 있어도 합법
 	void methodA() {
 		// 5번 라인의 전역변수의 값을 20에서 30으로 재정의함
 		// 아래 코드에서 int를 붙이면 지역변수가 되고 int를 붙이지 않으면
 		// 전역변수에 저장된 값 20이 30으로 덮어쓰기 됨
-		age = 30;
+		int age = 30;
+		Variable_1 v1 = new Variable_1();
+		System.out.println("after : "+v1.age);//20
 	}// end of methodA
 	// 14-15-16-5-18-19-7-11-12-21-22-23- 끝
 	public static void main(String[] args) {
@@ -18,6 +20,6 @@ public class Variable_1 {
 		System.out.println("before : "+v1.age);// 20이 출력됨
 		v1.methodA();// 이메소드를 경유하면 age가 30으로 변경됨
 		// 아래코드에서는 30이 출력됨	
-		System.out.println("after : "+v1.age);	
+		System.out.println("after : "+v1.age);// 20	
 	}// end of main 
 }////// end of Variable_1
--- dev_java/src/com/day4/Variable_3.java
+++ dev_java/src/com/day4/Variable_3.java
@@ -2,9 +2,13 @@ package com.day4;
 
 public class Variable_3 {
 	int age = 20;
-	void methodA(int age) {
+	//메소드의 파라미터 자리도 지역변수 자리 이다
+	//값은 호출할 때 결정됨
+	void methodA(int age) {//
+		// methodA를 벗어나면 자동 소멸됨
+		System.out.println("지역변수 ===> "+age);//50- automaic 자동소멸
 		age = 30;
-		System.out.println("methodA : "+age);
+		System.out.println("methodA 전역변수: "+this.age);//20
 	}
 	public static void main(String[] args) {
 		Variable_3 v1 = new Variable_3();
